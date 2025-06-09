3013e79e53f425ec893d2a607a5b214cbe440dea
Benjamin Oakes <boakes@spacebarlabs.com>
2025-06-09 13:55:11 -0500

    fix logic

--- chromium-extension/content_main.js
+++ chromium-extension/content_main.js
@@ -1,30 +1,28 @@
 // This script runs in the MAIN world and has access to navigator.share,
 // but it does not have access to extension APIs.
 
-if (window.navigator.share) {
-    // Override navigator.canShare.
-    window.navigator.canShare = function(data) {
-        return true; // Our custom share is always available.
-    };
+// Override navigator.canShare.
+window.navigator.canShare = function(data) {
+    return true; // Our custom share is always available.
+};
 
-    // Override navigator.share.
-    window.navigator.share = async function(data) {
-        console.log('Sharelette (Main World): Intercepted share action.');
-        
-        if (!data) return Promise.resolve();
+// Override navigator.share.
+window.navigator.share = async function(data) {
+    console.log('Sharelette (Main World): Intercepted share action.');
+    
+    if (!data) return Promise.resolve();
 
-        // Post a message to the window. The proxy script will pick this up.
-        window.postMessage({
-            type: 'SHARELETTE_SHARE_REQUEST',
-            data: {
-                url: data.url || window.location.href,
-                title: data.title || document.title,
-                text: data.text || ''
-            }
-        }, window.location.origin);
+    // Post a message to the window. The proxy script will pick this up.
+    window.postMessage({
+        type: 'SHARELETTE_SHARE_REQUEST',
+        data: {
+            url: data.url || window.location.href,
+            title: data.title || document.title,
+            text: data.text || ''
+        }
+    }, window.location.origin);
 
-        return Promise.resolve();
-    };
+    return Promise.resolve();
+};
 
-    console.log('Sharelette (Main World): Replaced navigator.share() and navigator.canShare().');
-}
+console.log('Sharelette (Main World): Replaced navigator.share() and navigator.canShare().');
